41b8da928cacc3a5b64fbd50a18128415c16ec53
George Lawrence <george.lawrence@duke.edu>
2020-01-28 16:17:52 -0500

    full push new update

--- public/core.js
+++ public/core.js
@@ -1,10 +1,25 @@
-var findAlums = angular.module('findAlums', []);
+var findAlums = angular.module('findAlums', [])
 
-findAlums.controller('mainController', ($scope, $http) => {
+.controller('mainController', ($scope, $http) => {
+
   let JSONattributes = ['lastName', 'firstName', 'gradYear', 'position', 'company', 'committee', 'boardPosition', 'dukeEmail', 'email', 'linkedin'];
   // this is a $scope object for table header
   $scope.humanAttributes = ['Last Name', 'First Name', 'Graduation Year', 'Current Position', 'Current Company',
                             'Committee(s)', 'Board Positions', 'Duke Email', 'Non-Duke Email', 'LinkedIn'];
+
+  $scope.committees = ['Annual Events', 'Campus Concerts', 'Coffeehouse', 'Downtown Duke', 'Duke Student Broadcasting', 'Freewater Presentations', 'Freewater Productions', 'Innovations', 'Jazz@', 'Joe College Concerts', 'LDOC','Major Attractions','Small Town Records','Speakers & Stage','Special Events','VisArts','WXDU'];
+
+  $scope.positions = ['Committee Chair', 'CFO', 'CMO', 'CTO', 'EVP', 'VPA', 'VPE', 'VPI', 'President'];
+
+  $scope.comm_checked = {};
+  for(var i = 0; i < $scope.committees.length; i++){
+    $scope.comm_checked[$scope.committees[i]] = false;
+  }
+
+  $scope.pos_checked = {};
+  for(var i = 0; i < $scope.positions.length; i++){
+    $scope.pos_checked[$scope.positions[i]] = false;
+  }
 
   // create attributes array which maps machine name to human name
   $scope.attributes = JSONattributes.map((element, index) => {
@@ -19,15 +34,92 @@ findAlums.controller('mainController', ($scope, $http) => {
       let rawData = res.data;
       // transform data into objects for sorting
       $scope.data = []; // will be array of person objects
+      console.log(rawData);
       rawData.forEach((person) => {
         var personObject = {};
         person.forEach((attribute, index) => {
           let attrName = $scope.attributes[index].value;
           personObject[attrName] = attribute === undefined ? '&nbsp;' : attribute;
         });
+        console.log(personObject)
         $scope.data.push(personObject);
       });
     }).catch((err) => {
       console.log(err);
     });
+
+    $scope.within = function(){
+        var yr = $scope.yrLimit;
+        return function(item){
+            if(yr === undefined){
+                return true;
+            }
+            else if (yr.min == null && yr.max == null){
+              return true;
+            }
+            else if (yr.min == null){
+                return item['gradYear'] <= yr.max;
+            } else if (yr.max == null){
+                return item['gradYear'] >= yr.min;
+            }
+          return item['gradYear'] >= yr.min && item['gradYear'] <= yr.max;
+        }
+    }
+
+    $scope.isPosChecked = function(){
+        return function(item){
+          var allFalse = true;
+
+          for(var i = 0; i < $scope.positions.length; i++){
+            if($scope.pos_checked[$scope.positions[i]]){
+              allFalse = false;
+            }
+          }
+
+          if(allFalse){
+            return true;
+          }
+
+          var posStr = item["boardPosition"];
+          var ret = false;
+          if(posStr != ""){
+            var poses = posStr.split(", ");
+            poses.forEach((pos) =>{
+              if ($scope.pos_checked[pos] && $scope.pos_checked[pos]!=undefined){
+                ret = true;
+              }
+            });
+          }
+          return ret;
+        }
+    }
+
+    $scope.isCommChecked = function(){
+        return function(item){
+          var allFalse = true;
+
+          for(var i = 0; i < $scope.committees.length; i++){
+            if($scope.comm_checked[$scope.committees[i]]){
+              allFalse = false;
+            }
+          }
+
+          if(allFalse){
+            return true;
+          }
+
+          var commStr = item["committee"];
+          var ret = false;
+          if(commStr != ""){
+            var comms = commStr.split(", ");
+            comms.forEach((comm) =>{
+
+              if ($scope.comm_checked[comm] && $scope.comm_checked[comm]!=undefined){
+                ret = true;
+              }
+            });
+          }
+          return ret;
+        }
+    }
 });
--- server.js
+++ server.js
@@ -32,52 +32,107 @@ app.listen(port, () => {
   console.log(`App listening on port ${port}`);
 });
 
+// If modifying these scopes, delete token.json.
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = 'token.json';
+
+// Load client secrets from a local file.
+fs.readFile('credentials.json', (err, content, res) => {
+  if (err) return console.log('Error loading client secret file:', err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), findAllAlums, res);
+});
+
 /**
- * Authorize call to Google API (code courtesy of Google Sheets docs)
- * @param {Object} credentials 
- * @param {Function} callback 
- * @param {Object} res 
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback, res) {
-  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  const {client_secret, client_id, redirect_uris} = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
       client_id, client_secret, redirect_uris[0]);
 
-  // finish setting up credentials, call callback
-  var token = {
-    access_token: process.env.access_token,
-    refresh_token: process.env.refresh_token,
-    scope: 'https://www.googleapis.com/auth/spreadsheets.readonly',
-    token_type: 'Bearer',
-    expiry_date: process.env.expiry_date
-  };
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback, res);
+    oAuth2Client.setCredentials(JSON.parse(token));
+    callback(oAuth2Client, res);
+  });
+}
 
-  oAuth2Client.setCredentials(token);
-  callback(oAuth2Client, res);
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback, res) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: 'offline',
+    scope: SCOPES,
+  });
+  console.log('Authorize this app by visiting this url:', authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question('Enter the code from that page here: ', (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err) return console.error('Error while trying to retrieve access token', err);
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log('Token stored to', TOKEN_PATH);
+      });
+      callback(oAuth2Client, res);
+    });
+  });
 }
 
 /**
  * Find all alumni via a call to Google Sheets API. Send response
  * to client via callback when done.
- * @param {Object} auth 
- * @param {Object} res 
+ * @param {Object} auth
+ * @param {Object} res
  */
-function findAllAlums(auth, res) {
+ function findAllAlums(auth, res) {
   const sheets = google.sheets({version: 'v4', auth});
   sheets.spreadsheets.values.get({
     spreadsheetId: '1alP9t4yeCx537NEISGLjpijIeUmisUqAh-LcPQCSUyA',
     range: 'Alumni Info!B2:K245',
-  }, (err, res2) => {
-    if (err) res.status(500);
-    res.status(200).send(res2.data.values);
+}, (err, res2) => {
+    if (err) return console.log('The API returned an error: ' + err);
+    const rows = res2.data.values;
+    if (rows.length) {
+      //console.log(res);
+      //console.log(res2)
+      var padded = padRows(rows);
+      //console.log(padded);
+      res.status(200).send(padded);
+    } else {
+      console.log('No data found.');
+    }
   });
 }
 
-// Put all alumni info into one string (for ease of printing)
-function printAlumInfo(row) {
-  var res = "";
-  row.forEach((attr) => {
-    res += attr === undefined ? " " : attr + " ";
+function padRows(rows){
+  var ret = [];
+  rows.forEach((row) => {
+    var tmp = [];
+    row.forEach((info) => {
+      tmp.push(info);
+    });
+    for(var i = 0; i < 10 - row.length; i++){
+      tmp.push('');
+    }
+    ret.push(tmp);
   });
-  return res;
+  return ret;
 }
